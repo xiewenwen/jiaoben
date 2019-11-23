0a6485f3bcf5a75cb1af55e88e08a0e919cdc1f6
xiewenwen <1121099970@qq.com>
2019-11-24 00:22:00 +0800

    YUNXING

---
+++ src/test/java/com/mayday.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.testng.annotations.Test;
+import org.testng.xml.dom.Tag;
+
+public class mayday {
+    private  int num1;
+
+    @Test
+    public void test12(){
+        System.out.println("DDDDDD");
+    }
+
+}
